7e2cd30376fa246ef6410c73712fe5b9820973bc
Sarfaraz Hasan <sarfarazhasanfb@gmail.com>
2021-11-04 21:19:18 +0530

    Working Blocker completed

--- client/src/components/screens/BHotelBlocker.js
+++ client/src/components/screens/BHotelBlocker.js
@@ -1,49 +1,64 @@
-import React, {useState,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import DatePicker from "react-multi-date-picker";
 
-const HotelBlocker = ()=>{
-    const [isBlockedOn, setIsBlockedOn] = useState([]);
-    const email = localStorage.getItem("email");
-    useEffect(()=>{
-        fetch(`getBlockedDates?email=${email}`,{
-            method:'get',
-            headers:{
-                'Content-Type':'application/json'
-            }
-        })
-        .then((res)=>res.json())
-        .then((data)=>{
-            setIsBlockedOn(data[0].isBlockedOn);
-            console.log(data[0].isBlockedOn);
-        })
+const HotelBlocker = () => {
+  const [isBlockedOn, setIsBlockedOn] = useState(["December 09 2020"]);
+  const email = localStorage.getItem("email");
 
-    },[]);
-    
-    const Block = ()=>{
-        
-        fetch("/blockUnblock",{
-            method: 'post',
-            headers: {
-             'Content-Type': 'application/json',
-           },
-           body: JSON.stringify({
-             email,
-             isBlockedOn
-           }),
-         })
-         .then((res)=>res.json())
-         .then((data)=>{
-             console.log(data);
-         })
-    }
-    
-    
-    return(
-        <>
-        <DatePicker multiple value={isBlockedOn} onChange={setIsBlockedOn}/>
-        <botton className="btn waves-effect waves-light #1e88e5 blue darken-1" onClick={Block}>Submit</botton>
-        </>
-        )
-}
+  useEffect(() => {
+    fetch(`/getBlockedDates?email=${email}`, {
+      method: "get",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setIsBlockedOn(data[0].isBlockedOn);
+        console.log(data[0].isBlockedOn);
+        setIsBlockedOn(data[0].isBlockedOn);
+      });
+  }, []);
+
+  const Block = () => {
+    const arrOfDates = isBlockedOn.map((ms) => {
+      var dateWithTime = new Date(ms);
+
+      return new Date(dateWithTime.setHours(0, 0, 0, 0)).toDateString();
+    });
+    console.log(typeof arrOfDates);
+    fetch("/blockUnblock", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email,
+        isBlockedOn: arrOfDates,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  };
+
+  return (
+    <>
+      <DatePicker
+        multiple
+        value={isBlockedOn}
+        format="ddd MMM DD YYYY"
+        onChange={setIsBlockedOn}
+      />
+      <botton
+        className="btn waves-effect waves-light #1e88e5 blue darken-1"
+        onClick={Block}
+      >
+        Submit
+      </botton>
+    </>
+  );
+};
 
 export default HotelBlocker;
